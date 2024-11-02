6c28e301778806b4b0ce076a727d2395527a202a
Risclover <sara091592@gmail.com>
2024-11-02 15:01:15 -0700

    change front page content

--- components/projectdetails.tsx
+++ components/projectdetails.tsx
@@ -12,16 +12,16 @@ type Props = {
   };
 };
 
-function projectdetails({ children, projectInfo }: Props) {
+function projectdetails({ projectInfo }: Props) {
   return (
     <div
       id="about"
-      ref={headingsRef}
+      //   ref={headingsRef}
       className="mx-auto max-w-6xl w-full px-6 lg:px-5 py-10 lg:py-20 flex flex-row-reverse justify-between dark:text-gray-50 items-start text-slate-900"
     >
       <div className="max-w-6xl lg:max-w-3xl font-wotfard text-lg w-full lg:pl-6">
         <QuickFacts projectInfo={projectInfo} />
-        {children}
+        {/* {children} */}
       </div>
     </div>
   );
